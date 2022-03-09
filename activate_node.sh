d8dd94f4d4ace6cc7f8f321ea05f5f55d69bef37
Minsoo Song <sms0432@gmail.com>
2022-03-10 08:41:07 +0900

    fix activate_node.sh

--- activate_node.sh
+++ activate_node.sh
@@ -2,5 +2,5 @@
 
 # docker pull 127.0.0.1:5000/:0.2
 source /opt/ros/foxy/setup.bash && \
-source install/setup.bash && \
+source /angel_core/install/setup.bash && \
 ros2 run angel_ss angel_ss_temp angel_ss_version
